cec42c7aafd97d0bb394db18bc3c6267ec53f31e
Sabarehmani <sabarehmani287@gmail.com>
2024-08-19 10:55:34 -0700

    string

---
+++ string.py
@@ -0,0 +1,7 @@
+str1 = "This is a string. we are creating it in python"
+print(str1)
+str1 = "This is a string.\n we are creating it in python"
+print(str1)
+str1 ="Saba"
+str2 ="Rehmani"
+print(str1+str2)
