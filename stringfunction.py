030572ac9b1648b5344652df2e45605fbe2ca8de
Sabarehmani <sabarehmani287@gmail.com>
2024-08-22 07:33:25 -0700

    functions

--- stringfunction.py
+++ stringfunction.py
@@ -3,3 +3,12 @@ print(str.endswith(a))
 
 str = "I am studying python from shrada khapra"
 print(str.replace("o","a"))
+
+str = "I am studying python from shrada khapra"
+print(str.find("o"))
+
+str = "I am studying python from shrada khapra"
+print(str.count("python"))
+
+name= input("enter your name : ")
+print("length of your name is",len(name))
