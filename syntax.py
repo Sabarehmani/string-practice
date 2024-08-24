6ef21ae53ec86e4638c519a706ec354ff8a66aca
Sabarehmani <sabarehmani287@gmail.com>
2024-08-24 11:12:46 -0700

    conditions

---
+++ syntax.py
@@ -0,0 +1,26 @@
+age = 22
+
+if(age <= 18):
+    print("can vote and apply for license")
+
+light = "green"
+if(light == "red"):
+    print("stop")
+elif(light = "green"):
+    print("go")
+elif(light = "yellow"):
+    print("look")
+else:
+    print("light is broken")
+
+age = 15
+
+if(age >= 18):
+    print("can vote")
+else:
+    print("cannot vote")
+
+
+
+
+    
